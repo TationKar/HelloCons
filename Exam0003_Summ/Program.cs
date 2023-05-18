5b59069a730befb1374fc6e593db32750efdc3ef
Timur Karimov <tationkar@gmail.com>
2023-05-18 22:30:25 +0300

    add Random numbers

--- Exam0003_Summ/Program.cs
+++ Exam0003_Summ/Program.cs
@@ -1,6 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
-console.WriteLine(result);
+Console.WriteLine(result);
